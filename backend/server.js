b0d70c184c4908f854bcc9d21831505466367a74
sankha-deep-Bt <sankhadeep.howrah@gmail.com>
2024-08-24 17:09:15 +0530

    Deployment build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,18 +12,23 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to get json payload from req.body
 app.use(cookieParser()); // to parse incoming cookie from req.cookie
+
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Running");
-// });
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html  "));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
